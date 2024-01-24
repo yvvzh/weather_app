370602d608194cf5725d0cc97941956722200daa
yvvzh <135761301+yvvzh@users.noreply.github.com>
2024-01-24 19:56:12 +0100

    added weather logic

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const input = document.getElementById("input");
 const searchBtn = document.getElementById("search-btn");
+const background = document.getElementById("background");
 
-const url = "https://api.open-meteo.com/v1/meteofrance?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m";
+const url = "https://api.open-meteo.com/v1/meteofrance?latitude=43.31&longitude=3.42&current=temperature_2m,weather_code,wind_speed_10m";
 
 let output = {};
 
@@ -14,4 +15,28 @@ fetch(url)
             wind: data.current.wind_speed_10m,
         };
         console.log(output);
+    })
+    .then(() => {
+        backgroundSelector(output.weather);
+        console.log(output.weather);
     });
+
+function backgroundSelector(weather) {
+    switch (true) {
+        case weather >= 0 && weather <= 10:
+            background.src = "/assets/img/sunny.jpg";
+            break;
+        case weather > 10 && weather <= 19:
+            background.src = "/assets/img/cloudy.jpg";
+            break;
+        case weather > 19 && weather <= 28:
+            background.src = "/assets/img/rainy.jpg";
+            break;
+        case weather > 28 && weather <= 99:
+            background.src = "/assets/img/thunder.jpg";
+            break;
+        default:
+            background.src = "/assets/img/default_weather.jpg";
+            console.log("weather unavailable");
+    }
+}
